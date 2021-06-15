cd9f03d78cc5311df7fa8816de413f2a9e0cbea5
Calvin Loncaric <marvinx03@gmail.com>
2021-06-15 11:39:28 -0700

    Remove some dead code

--- src/sj3d/RenderSettings.java
+++ src/sj3d/RenderSettings.java
@@ -1,8 +1,5 @@
 package sj3d;
 
-import static sj3d.Material.SMOOTH;
-import static sj3d.Material.TEXTURED;
-
 /**
  *
  * A collection of settings for render modes and quality. The default settings
@@ -31,17 +28,6 @@ public final class RenderSettings {
      */
     int mblur = 0xFF000000;
 
-    /**
-     * The maximum shading mode for the renderer. No triangle will be rendered
-     * with more complex settings than this.
-     */
-    int shadeMode = SMOOTH | TEXTURED;
-
-    /**
-     * Enable or disable shadows
-     */
-    private boolean shadows = false;
-
     // Methods
 
     /**
@@ -57,15 +43,6 @@ public final class RenderSettings {
      */
     public void setAA(float factor) {
         aaFactor = factor;
-    }
-
-    /**
-     * Identical to calling <code>setAA((float)factor)</code>
-     *
-     * @param factor
-     */
-    public void setAA(double factor) {
-        setAA((float) factor);
     }
 
     /**
@@ -91,66 +68,12 @@ public final class RenderSettings {
     }
 
     /**
-     * Identical to calling <code>setMotionBlur((float)factor)</code>
-     *
-     * @param factor
-     */
-    public void setMotionBlur(double factor) {
-        setMotionBlur((float) factor);
-    }
-
-    /**
      * Determine whether motion blur is enabled in these settings.
      *
      * @return true if motion blur is enabled, or false otherwise
      */
     public boolean hasMotionBlur() {
         return mblur >> 24 < 0xFF;
-    }
-
-    /**
-     * Set whether a given shading mode will be allowed.
-     *
-     * @param mode
-     *            the shading mode
-     * @param flag
-     *            true to enable the mode, false to disable i
-     */
-    public void setShading(int mode, boolean flag) {
-        if (flag)
-            shadeMode |= mode;
-        else
-            shadeMode ^= mode;
-    }
-
-    /**
-     * Determine whether a given shading mode is enabled.
-     *
-     * @param mode
-     *            the mode to check
-     * @return true if <code>mode</code> is enabled, or false otherwise
-     */
-    public boolean hasShading(int mode) {
-        return (shadeMode & mode) != 0;
-    }
-
-    /**
-     * Set whether shadows will be rendered or not.
-     *
-     * @param flag
-     *            true to enable shadows, false to disable them
-     */
-    public void setShadows(boolean flag) {
-        shadows = flag;
-    }
-
-    /**
-     * Determine whether shadows are currently enabled.
-     *
-     * @return true if shadows are enabled, or false otherwise
-     */
-    public boolean hasShadows() {
-        return shadows;
     }
 
 }
