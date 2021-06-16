77cf4b73d3ccbbc9379f7f6d32e3fc280f6b6ac6
Calvin Loncaric <marvinx03@gmail.com>
2021-06-15 17:25:44 -0700

    Camera is not an Object3D
    In particular, the scale and rotation are meaningless.  Furthermore,
    getMatrix() has entirely different semantics: model matrices convert
    from model coordiantes to world coordinates, while camera matrices
    convert from world coordinates to camera coordinates.

--- src/sj3d/Camera.java
+++ src/sj3d/Camera.java
@@ -5,33 +5,35 @@ package sj3d;
  *
  * @see #getMatrix()
  */
-public final class Camera extends Object3D {
+public final class Camera {
 
+    private float posX, posY, posZ;
     private Vector lookAtVector, forward;
+    private boolean rebuildFlag = true;
+    private Matrix transform = Matrix.identity();
 
     public Camera() {
-
         posX = posY = 0;
         posZ = -2;
         lookAtVector = new Vector(0, 0, 0);
-        rebuildFlag = true;
-
     }
 
-    public Camera(float x, float y, float z, float lookX, float lookY,
-            float lookZ) {
-
+    public Camera(float x, float y, float z, float lookX, float lookY, float lookZ) {
         posX = x;
         posY = y;
         posZ = z;
         lookAtVector = new Vector(lookX, lookY, lookZ);
-        rebuildFlag = true;
+    }
 
+    public void setPos(float x, float y, float z) {
+        posX = x;
+        posY = y;
+        posZ = z;
+        rebuildFlag = true;
     }
 
     public void lookAt(float x, float y, float z) {
         lookAtVector = new Vector(x, y, z);
-        rebuildFlag = true;
     }
 
     public Vector getLookVector() {
@@ -44,29 +46,26 @@ public final class Camera extends Object3D {
     }
 
     public Matrix getMatrix() {
+        if (rebuildFlag) {
+            forward = lookAtVector.subtract(new Vector(posX, posY, posZ));
+            Vector up = new Vector(0, 1, 0);
+            Vector right = up.cross(forward);
+            up = right.cross(forward);
 
-        if (!rebuildFlag)
-            return transform;
+            forward.normalize();
+            up.normalize();
+            right.normalize();
 
-        forward = lookAtVector.subtract(new Vector(posX, posY, posZ));
-        Vector up = new Vector(0, 1, 0);
-        Vector right = up.cross(forward);
-        up = right.cross(forward);
+            transform = new Matrix(right, up, forward);
+            transform.data[0][3] = posX;
+            transform.data[1][3] = posY;
+            transform.data[2][3] = posZ;
 
-        forward.normalize();
-        up.normalize();
-        right.normalize();
-
-        transform = new Matrix(right, up, forward);
-        transform.data[0][3] = posX;
-        transform.data[1][3] = posY;
-        transform.data[2][3] = posZ;
-
-        transform.invert();
-        rebuildFlag = false;
+            transform.invert();
+            rebuildFlag = false;
+        }
 
         return transform;
-
     }
 
 }
